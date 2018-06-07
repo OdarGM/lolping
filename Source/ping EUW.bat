e9bf3736a43d1667f9bff89bed81e95258e59454
Odar <radoradev770@gmail.com>
2018-06-07 15:31:40 +0300

    Add files via upload

---
+++ Source/ping EUW.bat
@@ -0,0 +1,43 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAlVSAuQAE+1EbsQ5+n//NaUr0wRW+Y+aoPU3buxB/ED1Uznephj33lV+A==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAlVSAuQAES0A5EO4f7+086CsUYJW/IDW4Df36eyF+8B4UzqctYi2EZWl8dMTAtRcBWXfgY6ri5ng1PLMt+Z0w==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cd\
+title League Of Legends EUW
+color 0a
+cls
+echo "Your ping for EUW"
+ping 104.160.141.3
+echo "PING TEST COMPLETED!!!"
+echo "THANK YOU FOR USING MY SERVICE"
+pause
